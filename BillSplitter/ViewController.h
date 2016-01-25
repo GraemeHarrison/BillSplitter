ff47f656acb018940b5a1d56abb001d8b8511773
Graeme Harrison <gharrison365@gmail.com>
2016-01-24 21:29:20 -0500

    First commit

---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Graeme Harrison on 2016-01-24.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
