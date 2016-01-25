8523587cb22ada36c22f164616287f33d5e3b5dd
Graeme Harrison <gharrison365@gmail.com>
2016-01-24 22:34:01 -0500

    Completed main tasks, albeit using CGFloat instead of NSDecimalNumber... Will have to update in morning

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -12,6 +12,9 @@
 @property (strong, nonatomic) IBOutlet UITextField *billAmountText;
 @property (strong, nonatomic) IBOutlet UISlider *peopleSlider;
 @property (strong, nonatomic) IBOutlet UILabel *amountPerPerson;
+@property (strong, nonatomic) IBOutlet UILabel *numberOfPeople;
+@property (nonatomic, assign) CGFloat num;
+@property (nonatomic, assign) CGFloat amount;
 
 @end
 
@@ -19,8 +22,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.billAmountText becomeFirstResponder];
+    self.num = 2.00;
+    
 }
 - (IBAction)calculateSplitAmount:(UIButton *)sender {
+    CGFloat billAmount = [self.billAmountText.text integerValue];
+    self.amount = billAmount / self.num;
+    self.amountPerPerson.text = [NSString stringWithFormat:@"Amount Per Person: $%.2f", self.amount];
+}
+
+- (IBAction)numberOfPeople:(UISlider *)sender {
+    self.num = sender.value;
+    self.numberOfPeople.text = [NSString stringWithFormat:@"%0.f", self.num];
 }
 
 - (void)didReceiveMemoryWarning {
